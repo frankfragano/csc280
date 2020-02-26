5eef10a97bd417d7b2d12def9d1d476c21119cd5
frankfragano <ff2863a@american.edu>
2020-02-26 16:33:32 -0500

    first commit

---
+++ p1.py
@@ -0,0 +1,9 @@
+def main():
+	dof = ["Tuesday","Wednesday","Friday"]
+today_variable = input("What day of the week is it today?")
+if today_variable = dof
+print("Today you have class")
+else 
+print("Today you do not have class")
+if __name__=="__main__":
+	main()
